af38cc59bd1b4ee4ecf55e6254da420aae19446e
Priscillia <dezettrep@gmail.com>
2020-02-03 17:58:59 +0100

    homepage

--- index.php
+++ index.php
@@ -1,21 +1,55 @@
 <?php include_once("header.php"); ?>
 <section class="jumbotron" style="background-image: url(public/images/salad.jpg)">
-<h1>Cours de cuisine</h1>
+    <h1>Cours de cuisine</h1>
 
-<section class="button">
-    <a class="btn white">
-        <span class="text">Nos Ateliers</span>
-        <span class="line -right"></span>
-        <span class="line -top"></span>
-        <span class="line -left"></span>
-        <span class="line -bottom"></span>
-    </a>
+    <section class="button">
+        <a class="btn white">
+            <span class="text">Nos Ateliers</span>
+            <span class="line -right"></span>
+            <span class="line -top"></span>
+            <span class="line -left"></span>
+            <span class="line -bottom"></span>
+        </a>
+    </section>
+
+
+</section>
+
+<section id="container">
+    <div class="row">
+
+
+        <img src="public/images/card5.jpg" width="30%">
+
+
+        <div class="col">
+            <div class="card">
+                <img src="public/images/card2.jpg" width="100%">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac ligula nibh. Proin porttitor
+                    dapibus rutrum. Suspendisse sit amet erat lectus. Nulla a nisi egestas, imperdiet justo vitae,
+                    accumsan est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Etiam imperdiet placerat velit. Morbi tempor odio ligula, et gravida est porttitor at.
+                    Donec a sem turpis.</p>
+            </div>
+        </div>
+
+
+        <img src="public/images/card4.jpg" width="30%">
+
+
+    </div>
+    </div>
+
 </section>
 
 
+<section id="premium" style="background-image: url(public/images/spag.jpg)">
+
+    <h2>Lorem ipsum dolor sit amet</h2>
+    <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+
+
 </section>
-
-
 
 
 
