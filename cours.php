71c5d83f75136856df8473e38e5cffa5f80fe9d4
Mathilde <mathilde-simonin@laposte.net>
2020-02-03 15:27:37 +0100

    cours v2

--- cours.php
+++ cours.php
@@ -1,13 +1,10 @@
 <?php include_once "header.php" ?>
 
-<section id="jumbotron">
-    <div>
-        <img src="">
-    </div>
-    <div>
+<section id="jumbotron" style="background-image: url(public/images/background.png);">
+    <div class="container">
         <h1>Participez à nos cours</h1>
+        <button>Découvrir</button>
     </div>
-    
 </section>
 
 <?php include_once "footer.php"?>
