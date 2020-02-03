b73a28b2dcbbaa4c715b0b1cdf2eb467736f78e3
Mathilde <mathilde-simonin@laposte.net>
2020-02-03 14:11:48 +0100

    cours

---
+++ cours.php
@@ -0,0 +1,13 @@
+<?php include_once "header.php" ?>
+
+<section id="jumbotron">
+    <div>
+        <img src="">
+    </div>
+    <div>
+        <h1>Participez à nos cours</h1>
+    </div>
+    
+</section>
+
+<?php include_once "footer.php"?>
