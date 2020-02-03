ecbb4bd82791be3897e642d905d181927c9cdfcc
Priscillia <dezettrep@gmail.com>
2020-02-03 14:51:35 +0100

    headerv3

--- header.php
+++ header.php
@@ -13,12 +13,14 @@
     <div class="container">
 
         <nav class="navbar" style="display: inline-flex">
-        <ul style="display: flex">
+        <ul>
+
             <li><a href="#">Nos Ateliers</a></li>
             <li><a href="#">Nos Locaux</a></li>
-        </ul>
-            <a class="titre" href="#" >Cookup</a>
-            <ul style="display: flex">
+
+            <li> <a class="titre" href="#" >Cookup</a> </li>
+
+
                 <li><a href="#">Blog</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
