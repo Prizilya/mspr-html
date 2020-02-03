7b628a45d0cae739b7da5af6afc98b0cafa1aa6d
Priscillia <dezettrep@gmail.com>
2020-02-03 14:15:23 +0100

    headerv2

---
+++ header.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="public/css/app.css">
+
+
+    <title>CookUp</title>
+</head>
+
+<header>
+    <div class="container">
+
+        <nav class="navbar" style="display: inline-flex">
+        <ul style="display: flex">
+            <li><a href="#">Nos Ateliers</a></li>
+            <li><a href="#">Nos Locaux</a></li>
+        </ul>
+            <a class="titre" href="#" >Cookup</a>
+            <ul style="display: flex">
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+
+
+
+
+        </nav>
+    </div>
+</header>
