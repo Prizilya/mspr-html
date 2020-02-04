dbf755d47aa70d6edfa3d35b5af82942cd40ed58
Mathilde <mathilde-simonin@laposte.net>
2020-02-04 13:54:37 +0100

    start blog

---
+++ blog.php
@@ -0,0 +1,7 @@
+<?php include_once "header.php" ?>
+
+    <section id="cours-jumbotron" style="background-image: url(public/images/painepices.png); width: 100%;">
+
+    </section>
+
+<?php include_once "footer.php" ?>
