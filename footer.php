b58c7610f74e91446c8b26043aea2e53bb9a9b82
Priscillia <dezettrep@gmail.com>
2020-02-04 13:41:11 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 </main>
 <footer id="footer">
 
+  </div>
+<a><img src="public/images/facebook.svg" width="3%"></a>
+<a><img src="public/images/instagram-sketched.svg" width="3%"></a>
+<a><img src="public/images/meetup.svg" width="3%"></a>
 
-
-
-<p>site créé par...</p>
+<p>site créé par Mathilde Simon & Priscillia Dezettre | <a href="#">Mentions légales</a></p>
 
 
 
