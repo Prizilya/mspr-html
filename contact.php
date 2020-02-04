1367d991f475540f35e1def44e5b40b8ad35f8d2
Priscillia <dezettrep@gmail.com>
2020-02-04 18:08:14 +0100

    pagecontact

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php include_once ("header.php") ?>
+
+<section id="home-jumbotron" class="jumbotron" style="background-image: url(public/images/locaux6.jpg)">
+    <div class="title"><h1>Contactez-nous</h1></div>
+    <div class="contact">
+        <div class="left" style="background-image: url(public/images/locaux4.jpg);"></div>
+        <div class="right">
+            <div class="formbox">
+                <form>
+                    <p>Nom</p>
+                    <input type="text" name="" placeholder="">
+                    <p>Prénom</p>
+                    <input type="text" name="" placeholder="">
+                    <p>Votre message</p>
+                    <input type="text" name="" placeholder="">
+                    <input type="submit" name="" value="Envoyer">
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</section>
+
+
+
+<?php include_once ("footer.php") ?>
+
+
