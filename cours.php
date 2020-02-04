2fe98ff0872994d7d236fcdcc13e8acc65666389
Mathilde <mathilde-simonin@laposte.net>
2020-02-04 09:31:53 +0100

    card

--- cours.php
+++ cours.php
@@ -3,7 +3,7 @@
 <section id="jumbotron" style="background-image: url(public/images/background.png);">
     <div class="container">
         <h1>Participez à nos cours</h1>
-        <section class="button">
+        <div class="button">
             <a class="btn black">
                 <span class="text black">Découvrir</span>
                 <span class="line -right"></span>
@@ -11,8 +11,23 @@
                 <span class="line -left"></span>
                 <span class="line -bottom"></span>
             </a>
+        </div>
+        <div class="section">
+            <div class="card">
+                <div class="img">
+                    <img src="public/images/card1.jpg" alt="">
+                </div>
+                <div class="text">
+                    <h2>cours sur moi !</h2>
+                </div>
+                <div>
+                    Description blablabla
+                </div>
+                <button>Inscris- toi !</button>
+            </div>
         </section>
     </div>
 </section>
+
 
 <?php include_once "footer.php"?>
