17ee72fea3849ba287636665f476a1b26add8f2d
Mathilde <mathilde-simonin@laposte.net>
2020-02-05 13:54:30 +0100

    alt-ention !

--- cours.php
+++ cours.php
@@ -16,12 +16,13 @@
         <div class="cours-card" id="cours-card">
             <div class="right">
                 <div class="card">
-                    <img src="public/images/card1.jpg" alt="">
+                    <img src="public/images/card1.jpg" alt="Bols d'épices indiennes">
                     <div class="card-body">
-                        <h2>Cours sur moi !</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquam aliquid optio
-                            repellat
-                            sequi!</p>
+                        <h2>Cours de cuisine : Epices !</h2>
+                        <p>Apprenez à distinguer le goût de la saveur et découvrez quatre règles simples qui vous
+                            ouvriront la porte du monde des épices. Avec les cinq techniques de base enseignées, vous
+                            pourrez intégrer toutes les épices dans votre cuisine. Vous comprendrez pourquoi 10 recettes
+                            + 10 mélanges d’épices = 100 recettes.</p>
                         <a href="inscription.php" class="btn black">
                             <span class="text black">Inscris- toi !</span>
                             <span class="line -right"></span>
@@ -34,14 +35,15 @@
             </div>
 
             <div class="cours-card">
-                <div  class="right">
+                <div class="right">
                     <div class="card">
-                        <img src="public/images/maca.jpg" alt="">
+                        <img src="public/images/maca.jpg" alt="Macarons à la framboise">
                         <div class="card-body">
-                            <h2>Cours sur moi !</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquam aliquid optio
-                                repellat
-                                sequi!</p>
+                            <h2>Cours de cuisine : Macaron !</h2>
+                            <p>Ce petit bijou sucré n’a rien d’une mode éphémère, car il est tout simplement
+                                exceptionnel à déguster. Le macaron est naturellement très sucré et la framboise bien
+                                acide va venir contrer l'excès de sucre...Amandes et fruits rouges, un accord parfait
+                                non ? Un cours macaron et hop, vous verrez la vie en rooose !</p>
                             <a href="inscription.php" class="btn black">
                                 <span class="text black">Inscris- toi !</span>
                                 <span class="line -right"></span>
@@ -54,14 +56,15 @@
                 </div>
 
                 <div class="cours-card">
-                    <div  class="right">
+                    <div class="right">
                         <div class="card">
-                            <img src="public/images/card1.jpg" alt="">
+                            <img src="public/images/soupe2.jpg" alt="Soupe de légumes">
                             <div class="card-body">
-                                <h2>Cours sur moi !</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquam aliquid optio
-                                    repellat
-                                    sequi!</p>
+                                <h2>Cours de cuisine : Soupe !</h2>
+                                <p>Une cuillère pour mamaaaannnn, une cuillère pour... Avec ces recettes de soupes, les
+                                    petits comme les grands vont en redemander, ça on peut vous le garantir ! Rien de
+                                    telle qu'une bonne soupe réconfortante pour mettre tout le monde d'accord. Allez,
+                                    vous en reprendrez bien un peu, non ?</p>
                                 <a href="inscription.php" class="btn black">
                                     <span class="text black">Inscris- toi !</span>
                                     <span class="line -right"></span>
