ba3b17a298af1bc213f4822d92a4d66833feb129
Mathilde <mathilde-simonin@laposte.net>
2020-02-05 11:03:34 +0100

    blog v1

--- blog.php
+++ blog.php
@@ -18,7 +18,7 @@
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad aliquam aliquid optio
                                     spellat sequi!</p>
                                 <a class="btn black">
-                                    <span class="text black">Inscris- toi !</span>
+                                    <span class="text black">Découvrir</span>
                                     <span class="line -right"></span>
                                     <span class="line -top"></span>
                                     <span class="line -left"></span>
@@ -36,22 +36,111 @@
                 <div class="w30 boite margin">
 
                     <img src="public/images/macarons.jpg" alt="Micro" title="Le micro de Valentine">
-                    <h2>Comment réaliser des macarons framboises maisons</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <h3>Comment réaliser des macarons framboises maisons ?</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
 
                 </div>
                 <div class="w30 boite margin">
 
                     <img src="public/images/couple.jpg" alt="Micro" title="Le micro de Valentine">
-                    <h2>Les bienfaits de la cuisine en couple</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <h3>Les bienfaits de la cuisine en couple</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
 
                 </div>
                 <div class="w30 boite">
 
                     <img src="public/images/pasta.jpg" alt="Micro" title="Le micro de Valentine">
-                    <h2>Les pâtes maison, la meilleure recette</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <h3>Les pâtes maison, la meilleure recette</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="blog-section2">
+        <div class="container">
+            <div class="flex-container">
+                <div class="w30 boite margin">
+
+                    <img src="public/images/legumes.jpg" alt="Micro" title="Le micro de Valentine">
+                    <h3>Cuisinez les légumes de saison</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="w30 boite margin">
+
+                    <img src="public/images/epices.jpg" alt="Micro" title="Le micro de Valentine">
+                    <h3>L'art des épices chiliennes</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="w30 boite">
+
+                    <img src="public/images/vege.jpg" alt="Micro" title="Le micro de Valentine">
+                    <h3>Une lunch box 100% végé</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut laoreet tortor. Aliquam ex
+                        erat, elementum ut ultrices et, finibus nec nisl.</p>
+                    <div class="center">
+                        <a class="btn black">
+                            <span class="text black">Découvrir</span>
+                            <span class="line -right"></span>
+                            <span class="line -top"></span>
+                            <span class="line -left"></span>
+                            <span class="line -bottom"></span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
