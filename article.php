8256717b4fb33a3e0e52e7dc76c53b665b0af5fc
Mathilde <mathilde-simonin@laposte.net>
2020-02-06 10:10:48 +0100

    update article

---
+++ article.php
@@ -0,0 +1,61 @@
+<?php include_once "header.php" ?>
+
+    <section id="article-jumbotron">
+        <div class="container">
+            <h1>Comment réaliser des macarons framboise maison ?</h1>
+            <div class="center">
+                <img src="public/images/macarons.jpg" alt="Macarons à la framboise.">
+                <p>there is a little description</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="article-1">
+        <div class="container">
+            <div class="flex-container">
+                <div class="fl">
+                    <img src="public/images/macaron1.jpg" alt="Macarons à la framboise avec une rose violette.">
+                </div>
+                <div class="fr">
+                    <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
+                        sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une
+                        des
+                        sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison.
+                        Faites
+                        plaisir à
+                        votre famille en réalisant cette recette !
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="article-1">
+        <div class="w30">
+            <img src="public/images/macaron2.jpg" alt="Macarons à la framboise avec des petites framboises.">
+        </div>
+        <div class="w60">
+            <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
+                sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une des
+                sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison. Faites
+                plaisir à
+                votre famille en réalisant cette recette !
+            </p>
+        </div>
+    </section>
+
+    <section id="article-2">
+        <div class="w30">
+            <img src="public/images/macaron3.jpg" alt="Macarons à la framboise avec des petites framboises.">
+        </div>
+        <div class="w60">
+            <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
+                sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une des
+                sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison. Faites
+                plaisir à
+                votre famille en réalisant cette recette !
+            </p>
+        </div>
+    </section>
+
+<?php include_once "footer.php" ?>
