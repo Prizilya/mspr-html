0d6f521bc467c6c2db2de8071ea56488789ce8e7
Mathilde <mathilde-simonin@laposte.net>
2020-02-06 12:33:41 +0100

    article update

--- article.php
+++ article.php
@@ -1,60 +1,99 @@
 <?php include_once "header.php" ?>
 
+
     <section id="article-jumbotron">
         <div class="container">
             <h1>Comment réaliser des macarons framboise maison ?</h1>
+            <div class="imgtext">
+                <img src="public/images/macaronscarré.jpg" alt="Macarons à la framboise avec une rose violette.">
+                <p>Les macarons c’est pas facile à faire. Déjà il faut anticiper. Genre si t’as pas séparé tes
+                    blancs d’oeufs une semaine à l’avance t’es morte. Ensuite il faut de la patience, et un minimum
+                    de matériel. Depuis bien longtemps, j’avais donc abdiqué, et quand j’ai envie de macarons j’en
+                    achète.
+
+                    Souvent, c’est lors de mes nombreux allez-retours parisiens ou dès que je passe devant un grand
+                    nom de la pâtisserie je m’arrête et je teste quelques gourmandises, comme chez Ladurée, Gérard
+                    Mulot, Sadaharu Aoki ou dernièrement chez Pierre Hermé où avec Cathy nous nous somme fait notre
+                    quatre heures de gourmandes, avec un jus de fruit de chez Cojean (j’ai pas osé tester le jus
+                    d’herbes …).
+
+                    Et puis, un samedi, j’ai fait des macarons chez des amis, avec le livre de Mercotte sous les
+                    yeux, et l’iphone et son application “macarons”. Nous avons fait des macarons à la framboise et
+                    à la pistache et ils étaient délicieux et super beau ! Alors j’ai décidé de me relancer dans la
+                    réalisation de macarons dans ma p’tite cuisine.
+                </p>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="article-1">
+        <div class="container">
             <div class="center">
-                <img src="public/images/macarons.jpg" alt="Macarons à la framboise.">
-                <p>there is a little description</p>
+                <div class="center">
+                    <img src="public/images/macaron1.jpg" alt="Macaron">
+                </div>
+                <p>J’avais assez peur du résultat, ayant maintenant un four combiné vapeur et plus un four traditionnel,
+                    mais en enlevant la cassette d’eau, en le séchant bien et en le préchauffant 20 minutes, j’ai obtenu
+                    de très jolies coques de macarons, que j’ai garnis d’une ganache montée à la fraise. Trop contente
+                    de moi, j’ai refait des macarons le dimanche, parfumé avec une ganache montée chocolat blanc, pèche
+                    et fleur d’oranger. Ils étaient aussi très bon et étaient parfait pour commencer la semaine au
+                    bureau avec un café (ou un thé).
+
+                    Pour mes macarons, j’ai suivi tous les conseils de Mercotte, n’hésitez pas à aller sur son blog il y
+                    a vraiment toutes les infos pour réussir les macarons, ici vous avez vraiment le minimum. Pour le
+                    matériel, j’achète mes plaques perforées chez Demarle, mes colorants / poches à douilles / arômes
+                    chez cook-shop ou dans les boutiques spécialisées en ustensiles de cuisine.
+                </p>
             </div>
         </div>
     </section>
 
     <section id="article-1">
         <div class="container">
-            <div class="clearfix">
-                <div class="fl">
-                    <img src="public/images/macaron1.jpg" alt="Macarons à la framboise avec une rose violette.">
-                </div>
-                <div class="fr">
-                    <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
-                        sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une
-                        des
-                        sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison.
-                        Faites
-                        plaisir à
-                        votre famille en réalisant cette recette !
-                    </p>
-                </div>
+            <div class="center">
+                <img src="public/images/macaron3.jpg" alt="Macarons à la framboise avec des petites framboises.">
+
+                <p>Coques de macarons (recette sans meringue Italienne) <br>
+                    90 g de blancs d’œufs  (séparés 7 jours à l’avance, conservés au réfrigérateur et sorti du réfrigérateur 3 heures avant de préparer les macarons) <br>
+                    220 g de sucre glace <br>
+                    120 g de poudre d’amandes<br>
+                    20 g de sucre en poudre<br>
+                    1/2 cuillère à café de colorant en poudre (rouge pour les fraises, jaune pour les pèches / fleur d’oranger)<br>
+                    <br>
+                    1. Préchauffez le four à 160 °.<br>
+                    <br>
+                    2. Dans un mixeur, mixer quelques instants la poudre d’amande et le sucre glace pour éviter de les tamiser.<br>
+                    <br>
+                    3. Monter les blancs en neige ferme avec une pincée de sel. Ajouter progressivement le sucre en poudre et le colorant. Les blancs doivent former des becs d’oiseau quand on enlève le fouet.<br>
+                    <br>
+                    4. Ajouter le mélange poudre d’amande / sucre glace et mélanger avec une corne (bien plus pratique qu’une maryse) en un mouvement de bas en haut en partant du centre du bol. L’appareil doit être lisse et brillant, il doit faire le ruban sans être liquide, juste souple.<br>
+                    <br>
+                    5. Remplir une poche a douille lisse avec la préparation, et dresser les macarons sur un tapis en silicone ou du papier cuisson siliconé, déposé sur une grille perforée.<br>
+                    <br>
+                    6. Enfourner pour 13 à 16 minutes de cuisson, selon votre four et la taille des macarons. Les miens étaient de taille moyenne, et ils ont cuit 16 minutes. Laissez légèrement refroidir avant de retirer les macarons de la feuille.<br>
+                    <br>
+                    7. Laisser refroidir les coques avant de les garnir.<br>
+                    <br>
+                    Ganaches montées parfumées<br>
+                    50 g de chocolat blanc à pâtisserie (j’utilise la couverture Ivoire Valrhona)<br>
+                    25 g de crème fleurette entière<br>
+                    3 g de miel<br>
+                    75 g de crème fleurette froide<br>
+                    1 belle cuillère à café d’arôme (fraise pour les premiers, puis pèche et fleur d’oranger pour les seconds)<br>
+                    <br>
+                    1. Faire chauffer la crème et le miel et y ajouter l’arôme.<br>
+                    <br>
+                    2. Y faire fondre le chocolat en morceaux et mélanger pour le faire fondre.<br>
+                    <br>
+                    3. Ajouter les 75 g de crème restant et mélanger. Conserver au réfrigérateur pour faire refroidir la ganache.<br>
+                    <br>
+                    4. Quand la ganache est froide, la monter en chantilly puis remplir une poche à douille avec et garnir les macarons.<br>
+                    <br>
+                    5. Ranger ensuite les macarons au réfrigérateur, ils sont meilleurs au moins deux jours après les avoir assemblés …<br>
+                </p>
+
             </div>
-        </div>
-    </section>
-
-    <section id="article-1">
-        <div class="w30">
-            <img src="public/images/macaron2.jpg" alt="Macarons à la framboise avec des petites framboises.">
-        </div>
-        <div class="w60">
-            <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
-                sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une des
-                sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison. Faites
-                plaisir à
-                votre famille en réalisant cette recette !
-            </p>
-        </div>
-    </section>
-
-    <section id="article-2">
-        <div class="w30">
-            <img src="public/images/macaron3.jpg" alt="Macarons à la framboise avec des petites framboises.">
-        </div>
-        <div class="w60">
-            <p>Le macaron est une des sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils
-                sont fait maison. Faites plaisir à votre famille en réalisant cette recette ! Le macaron est une des
-                sucreries les appréciées en France. Ils sont encore meilleurs lorsqu'ils sont fait maison. Faites
-                plaisir à
-                votre famille en réalisant cette recette !
-            </p>
         </div>
     </section>
 
